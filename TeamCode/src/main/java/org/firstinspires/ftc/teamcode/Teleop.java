6bb933aa5f43e20099ccbe62ca6abad6a37cc6d4
wykzimme <bryce.e.zimmerman@gmail.com>
2025-05-26 13:24:20 -0600

    added teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name ="teleop", group = "2024-25 SP")
+//naming it
+public class Teleop extends OpMode {
+    chaseintialization driveteleop = new chaseintialization();
+    armintialization LineraMecanizmeteleop = new armintialization();
+
+    @Override
+    public void init() {
+
+        //calling hardwareMap
+        driveteleop.init(hardwareMap);
+        LineraMecanizmeteleop.init(hardwareMap);
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        //seting powers
+        driveteleop.topleft.setPower(-gamepad1.right_stick_y / 1.1 + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        driveteleop.backleft.setPower(gamepad1.right_stick_y / 1.1 + -gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
+        driveteleop.topright.setPower(-gamepad1.right_stick_y * 1.1 + gamepad1.left_stick_x * 1.2 -gamepad1.right_stick_x);
+        driveteleop.backright.setPower(gamepad1.right_stick_y * 1.1 + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        LineraMecanizmeteleop.leftliner.setPower(-gamepad2.right_stick_y);
+        LineraMecanizmeteleop.rightliner.setPower(gamepad2.right_stick_y);
+        LineraMecanizmeteleop.forwardliner.setPower(gamepad2.left_stick_y);
+
+    }
+}
