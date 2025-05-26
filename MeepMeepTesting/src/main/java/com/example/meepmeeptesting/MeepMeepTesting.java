118b9e04538f763a3bd29b4e47b775a77eab82cf
wykzimme <bryce.e.zimmerman@gmail.com>
2025-05-26 13:18:59 -0600

    added auto 1

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -39,48 +40,15 @@ public class MeepMeepTesting {
                                 new Pose2d(-47, 60, Math.toRadians(270)), // Target pose
                                 Math.toRadians(270)                      // Tangent angle
                         )
-                        .forward(11)
                         .setReversed(true)
-                        .splineToLinearHeading(
-                                new Pose2d(-0.8, 36, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .splineToLinearHeading(
-                                new Pose2d(-44, 48, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .back(15)
-                        .forward(7)
-                        .splineToLinearHeading(
-                                new Pose2d(-0.8, 36, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .splineToLinearHeading(
-                                new Pose2d(-44, 48, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .back(15)
-                        .forward(7)
-                        .splineToLinearHeading(
-                                new Pose2d(-0.8, 36, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .splineToLinearHeading(
-                                new Pose2d(-44, 48, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
-                        .setReversed(true)
-                        .back(15)
-                        .forward(7)
-                        .splineToLinearHeading(
-                                new Pose2d(-0.8, 36, Math.toRadians(270)), // Target pose
-                                Math.toRadians(270)                      // Tangent angle
-                        )
+                        .back(3)
+                        .strafeTo(new Vector2d(-.08, 36))//2
+                        .strafeTo(new Vector2d(-47, 63))
+                        .strafeTo(new Vector2d(-1.3, 36))//3
+                        .strafeTo(new Vector2d(-47, 63))
+                        .strafeTo(new Vector2d(-1.5, 36))//4
+                        .strafeTo(new Vector2d(-47, 63))
+                        .strafeTo(new Vector2d(-1.8, 36))//5
                        .build());
         myBot.setDimensions(16, 16.5);
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto15specimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto15specimen.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Autonomous(name = "SP 5 specimen", group = "2024-25 SP")
 public class Auto15specimen extends LinearOpMode{
 
+    
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode(){
+
+        waitForStart();
 
     }
 }
