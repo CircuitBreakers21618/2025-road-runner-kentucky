427b28651f25cc83f647b8bed88c5652f5233787
wykzimme <bryce.e.zimmerman@gmail.com>
2025-06-03 19:23:48 -0600

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -24,42 +24,42 @@ public class Teleop extends OpMode {
     public void loop() {
 
         //seting powers
-        driveteleop.leftFront.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
-        driveteleop.leftBack.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.3+ gamepad1.right_stick_x);
-        driveteleop.rightFront.setPower(gamepad1.right_stick_y + gamepad1.left_stick_x * 1.2 +gamepad1.right_stick_x);
-        driveteleop.rightBack.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
+        driveteleop.leftFront.setPower(gamepad1.right_stick_y  + gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
+        driveteleop.leftBack.setPower(-gamepad1.right_stick_y  + gamepad1.left_stick_x * 1.1+ gamepad1.right_stick_x);
+        driveteleop.rightFront.setPower(gamepad1.right_stick_y  + gamepad1.left_stick_x * 1.1 +gamepad1.right_stick_x);
+        driveteleop.rightBack.setPower(-gamepad1.right_stick_y  + gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
         LineraMecanizmeteleop.leftliner.setPower(-gamepad2.right_stick_y);
         LineraMecanizmeteleop.rightliner.setPower(gamepad2.right_stick_y);
         LineraMecanizmeteleop.forwardliner.setPower(gamepad2.left_stick_y);
 
-
-        if (gamepad2.y){
-            LineraMecanizmeteleop.toparm1.setPosition(1);
-
-        }
-
-        if (gamepad2.y){
-            LineraMecanizmeteleop.toparm2.setPosition(-1);
-
-        }
-        if (gamepad2.a){
-            LineraMecanizmeteleop.toparm1.setPosition(1);
-
-        }
-
-        if (gamepad2.a){
-            LineraMecanizmeteleop.toparm2.setPosition(-1);
-
-        }
-        if (gamepad2.b){
-            LineraMecanizmeteleop.topclaw.setPosition(1);
-
-        }
-
-        if (gamepad2.x){
-            LineraMecanizmeteleop.topclaw.setPosition(0);
-
-        }
+//
+//        if (gamepad2.y){
+//            LineraMecanizmeteleop.toparm1.setPosition(1);
+//
+//        }
+//
+//        if (gamepad2.y){
+//            LineraMecanizmeteleop.toparm2.setPosition(-1);
+//
+//        }
+//        if (gamepad2.a){
+//            LineraMecanizmeteleop.toparm1.setPosition(1);
+//
+//        }
+//
+//        if (gamepad2.a){
+//            LineraMecanizmeteleop.toparm2.setPosition(-1);
+//
+//        }
+//        if (gamepad2.b){
+//            LineraMecanizmeteleop.topclaw.setPosition(1);
+//
+//        }
+//
+//        if (gamepad2.x){
+//            LineraMecanizmeteleop.topclaw.setPosition(0);
+//
+//        }
 
 
 
