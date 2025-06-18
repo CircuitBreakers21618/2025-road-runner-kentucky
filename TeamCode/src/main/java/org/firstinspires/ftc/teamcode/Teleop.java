2e590d2f2a7d4e7cf00f432f2d6f1677bc819bfb
wykzimme <bryce.e.zimmerman@gmail.com>
2025-06-17 20:59:13 -0600

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -47,10 +47,10 @@ public class Teleop extends OpMode {
     public void loop() {
 
         //seting powers
-        driveteleop.leftFront.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
-        driveteleop.leftBack.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
-        driveteleop.rightFront.setPower(gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
-        driveteleop.rightBack.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
+        driveteleop.leftFront.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
+        driveteleop.leftBack.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        driveteleop.rightFront.setPower(-gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        driveteleop.rightBack.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.right_stick_x);
         LineraMecanizmeteleop.leftliner.setPower(-gamepad2.right_stick_y);
         LineraMecanizmeteleop.rightliner.setPower(gamepad2.right_stick_y);
         LineraMecanizmeteleop.forwardliner.setPower(gamepad2.left_stick_y);
@@ -83,6 +83,9 @@ public class Teleop extends OpMode {
             LineraMecanizmeteleop.bottomrotator.setPosition(1);
         }
 
+        if(gamepad2.right_bumper){
+            LineraMecanizmeteleop.bottomrotator.setPosition(.5);
+        }
 
         if (gamepad2.y) {
             LineraMecanizmeteleop.topclaw.setPosition(0.6);
