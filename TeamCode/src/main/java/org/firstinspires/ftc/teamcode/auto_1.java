f8f37e34bb9276e1530a7fd3070e6be93f2cc45b
wykzimme <bryce.e.zimmerman@gmail.com>
2025-06-20 13:23:16 -0600

    fixed ato

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_1.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static android.os.SystemClock.sleep;
+
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
@@ -9,35 +11,38 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous
 public class auto_1 extends LinearOpMode {
     armintialization LineraMecanizmeteleop = new armintialization();
+    chaseintialization driveato = new chaseintialization();
 
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
 
-
-
-
-        Pose2d beginPose = new Pose2d(-71, -5, 0);
-
-        //calling hardwareMap
-        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         LineraMecanizmeteleop.init(hardwareMap);
+        driveato.init(hardwareMap);
 
         waitForStart();
 
-        if (isStopRequested()) return;
-        Actions.runBlocking(
-                drive.actionBuilder(beginPose)
-                        .afterTime(.5,LineraMecanizmeteleop.hang_specimen())
-                        .waitSeconds(.75)
-                        .lineToX(-38)
-                        .waitSeconds(2)
-                        .setReversed(true)
-                        .splineToLinearHeading(
-                                new Pose2d(-71, -71, Math.toRadians(0)), // Target pose
-                                Math.toRadians(180)                      // Tangent angle
-                        )
-                        .build());
+        LineraMecanizmeteleop.topclaw.setPosition(0.15);
+        sleep(500);
+        LineraMecanizmeteleop.toparm1.setPosition(0.7);//moves to hang position
+        LineraMecanizmeteleop.toparm2.setPosition(0.06);
+        driveato.moveforward(1, 1000);
+        driveato.stopmotor();
+        LineraMecanizmeteleop.rightliner.setPower(-.5);
+        LineraMecanizmeteleop.leftliner.setPower(.5);
+        sleep(2000);
+        LineraMecanizmeteleop.rightliner.setPower(0);
+        LineraMecanizmeteleop.leftliner.setPower(0);
+        LineraMecanizmeteleop.topclaw.setPosition(0.6);
+        driveato.moveforward(-1, 500);
+        driveato.stopmotor();
+        driveato.RightTurn(.5, 750);
+        driveato.stopmotor();
+        driveato.moveforward(1, 750);
+        driveato.stopmotor();
+        driveato.LeftTurn(.5, 750);
+        driveato.stopmotor();
+        driveato.moveforward(-1, 500);
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chaseintialization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chaseintialization.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -16,6 +18,44 @@ public class chaseintialization {
         rightFront = hwMap.get(DcMotor.class, "rightFront");
         leftBack = hwMap.get(DcMotor.class, "leftBack");
         rightBack = hwMap.get(DcMotor.class, "rightBack");
+    }
+
+
+    public void RightTurn(double power, long time) throws InterruptedException {
+        leftBack.setPower(power);
+        rightBack.setPower(-power);
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        //how long it waits
+        sleep(time);
+
+    }
+    public void LeftTurn(double power, long time) throws InterruptedException {
+        leftBack.setPower(-power);
+        rightBack.setPower(power);
+        leftFront.setPower(-power);
+        rightFront.setPower(-power);
+        //how long it waits
+        sleep(time);
+
+    }
+
+    public void stopmotor(){
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+
+    }
+
+    public void moveforward(double power, long time) throws InterruptedException {
+        leftBack.setPower(-power);
+        rightBack.setPower(power);
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        //how long it waits
+        sleep(time);
+
     }
 }
 
